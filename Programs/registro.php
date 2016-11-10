5bfe5d2dafa5a7e08d9a00c867487fe027bf7706
Alma Sánchez <Alma Snchez>
2016-11-10 04:44:28 -0600

    Funcionando registro alumno

--- Programs/registro.php
+++ Programs/registro.php
@@ -1,2 +1,11 @@
 <?php
-?>
+  if(!empty ($nombre=$_POST['name']))
+  {
+    echo'Registro concluido exitosamente,'.$nombre;
+    echo'<br/><a href="login.php">Ingresar</a>';
+  }
+  else
+  {
+    echo'Algo falló con el registro, no se concluyó';
+  }
+  ?>
