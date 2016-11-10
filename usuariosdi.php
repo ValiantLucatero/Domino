5632e2cda31fbd3ae9d1a92c9937a08bdcd685e6
Diana <dianamendezavalos@gmail.com>
2016-11-10 03:52:05 +0000

    Replace usuariosdi.php

--- usuariosdi.php
+++ usuariosdi.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+		<link href="css/bootstrap.min.css" rel="stylesheet"/>
 		<link rel="stylesheet/less" type="text/css" href="dise.less"/>		
 		<script src="less.min.js" type="text/javascript"></script>
 	</head>
